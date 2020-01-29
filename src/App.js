b7ab46b22509e3f9a529ba399e1c9c892cbda9c7
Jawad <rjava721@gmail.com>
2020-01-29 15:41:40 -0600

    implement displaying a new post upon clicking on post

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 //import Header from './Header';
-import Status from './Status';
-import Timeline from './Timeline';
+import StatusForm from './StatusForm';
+//import Timeline from './Timeline';
 import Post from './Post';
 
 // import Comment from './Comment';
 import './App.css';
 
-// current functionality objectives
-// link my current user to my post
-// be able to post an item according to each user
+// update your data
+// you are rendering your stuff through setState which updates your data
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
       currentUser: 'Jawad',
-      users : ['jawad', 'Skywalker'],
-      userPosts : []
+      users: ['jawad', 'Skywalker'],
+
+      userPosts: [],
+      formValue: '',
     }
   }
 
-  createPost = (event) => {
-    console.log('hi');
-  }
+  // Header================================
 
   updateCurrentUser = (event) => {
     {/* inside of header, on change , when the name is selected, grab it with event.name and change state to it */}
@@ -34,20 +33,55 @@ class App extends Component {
     console.log('value of this.state.currentUser after this.setState ' + this.state.currentUser);
   }
 
-  render() {
-    // inside of Header, we need to grab the value of the select tag 
-    // in order to set our currentUser
-    // => we change Header from class to function
+  // StatusForm
 
+  handleFormSubmit = (event) => {
+    let userPosts = this.state.userPosts;
+    let formValue = this.state.formValue;
+
+    // prevent the page reload
+    console.log('form submit function called');
+    event.preventDefault();
+
+    // take the state array and store the formtextcontnet inside of it.
+    userPosts.push(formValue);
+    console.log(userPosts);
+
+    // empty the form input box
+    this.setState({ formValue : '' });
+
+    // then map through the array and then create a post inside the render function
+    // the function gets called inside of the render
+    
+  }
+
+  //call it renderPost
+  createPost = () => {
+    return (this.state.userPosts.map((post) => { return <Post currentUser={this.state.currentUser} formContent={post} /> }))
+  }
+
+  updateState = (event) => {
+    // update the state
+    // push the texcontent inside of a state array that is meant to store all posts
+    // and clear the input box
+    // map through the array
+    // create a new post for each array element
+    
+    this.setState({ formValue : event.target.value});
+    // this.setState({ formValue: ''});
+  }
+
+  render() {
+    
     return (
       <div className='main-container'>
         <Header currentUser={this.state.currentUser} users={this.state.users} onChange={this.updateCurrentUser}/>
-        <Status currentUser={this.state.currentUser} users={this.state.users} onClick={this.createPost}/>
-        <Timeline />
-      </div>
-      
+        <StatusForm currentUser={this.state.currentUser} handleFormSubmit={this.handleFormSubmit} updateState={this.updateState} formValue={this.state.formValue} />
+        {this.createPost()}
+      </div>  
     )
   }
+
 }
 
 let Header = (props) => {
--- src/Post.js
+++ src/Post.js
@@ -14,12 +14,12 @@ class Post extends Component {
         <div className='post-container'>
         <img src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwirj-7h4KbnAhXHpZ4KHYeuBcgQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.onlinewebfonts.com%2Ficon%2F513928&psig=AOvVaw3PLCpl4ZwhF2_orwYfeUz7&ust=1580316737287677" alt="prfl"/>
           <div className='top'>
-            <h4>Name</h4>
+            <h4>{this.props.currentUser} NEW POST</h4>
             <h4>Time</h4>
           </div>
 
           <div className='post-content'>
-            <p>The post content goes here.</p>
+            <p>{this.props.formContent}</p>
             <p>1 Like 1 comment</p>
           </div>
           <div className='post-interaction-box'>
